1b735e8e23f5d5658374272d189025b9c5e84cc8
qushaowei <qushaowei111@outlook.com>
2019-12-03 08:39:35 +0800

    addGoods,deleteGoods,updateGoods

--- OceanFish/app/src/main/java/ouc/cs/oceanfish/dao/GoodsDao.java
+++ OceanFish/app/src/main/java/ouc/cs/oceanfish/dao/GoodsDao.java
@@ -1,31 +1,117 @@
 package ouc.cs.oceanfish.dao;
 import ouc.cs.oceanfish.po.Goods;
+import ouc.cs.oceanfish.util.DbUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class GoodsDao {
 
-    //增加物品信息，无条件增加到待审核表
-    public boolean addGoods(Goods goods){
-        return true;
-    }
+    //增加物品信息
+    public int addGoods(Goods goods){
+        Connection conn = DbUtil.getConnection();
+        String sql = "insert into goods(userId,name,picture,price,contactWay,address," +
+                "describe,type,soldStatus) value (?,?,?,?,?,?,?,?,?)";
+        Statement st = null;
+        int result = 0;
+        try {
+            st = conn.createStatement();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1,goods.getUserId() );
+            ps.setString( 2,goods.getName());
+            ps.setString( 3,goods.getPicture());
+            ps.setDouble( 4,goods.getPrice());
+            ps.setString( 5,goods.getContactWay());
+            ps.setString( 6,goods.getAddress());
+            ps.setString( 7,goods.getDescribe());
+            ps.setString( 8,goods.getType());
+            ps.setString( 9,goods.getSoldStatus());
 
-    //增加物品信息到通过审核表
-    public boolean addGoodsSecond(Goods goods){
-        return true;
+            result = ps.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                if (st != null) {
+                    st.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
     //删除物品信息，通过匹配id删除
-    public boolean deleteGoods(int id){
-        return true;
+    public int deleteGoods(int id){
+        Connection conn = DbUtil.getConnection();
+        String sql = "delete from goods where id = ?";
+        Statement st = null;
+        int result = 0;
+        try {
+            st = conn.createStatement();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            result = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (st != null) {
+                    st.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
     //修改物品信息，通过id搜索，修改为传送的信息
-    public boolean updateGoods(int id, Goods goods){
-        return true;
+    public int updateGoods(int id, Goods goods){
+        Connection conn = DbUtil.getConnection();
+        String sql = "update goods set userId = ? , name = ? , picture = ? , price = ? , " +
+                "contactWay = ? , address = ? , describe = ? , type = ? , soldStatus = ? where " +
+                "id = ?";
+        Statement st = null;
+        int result = 0;
+        try {
+            st = conn.createStatement();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1,goods.getUserId() );
+            ps.setString( 2,goods.getName());
+            ps.setString( 3,goods.getPicture());
+            ps.setDouble( 4,goods.getPrice());
+            ps.setString( 5,goods.getContactWay());
+            ps.setString( 6,goods.getAddress());
+            ps.setString( 7,goods.getDescribe());
+            ps.setString( 8,goods.getType());
+            ps.setString( 9,goods.getSoldStatus());
+            ps.setInt( 10,goods.getId());
+
+            result = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (st != null) {
+                    st.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
     //通过id搜索唯一的物品
-    public boolean findGoodsById(int id){
-        return true;
+    public Goods findGoodsById(int id){
+        Goods goods = null;
+        return goods;
     }
 
     //通过name搜索一种物品
